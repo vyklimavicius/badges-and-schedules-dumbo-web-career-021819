ba6d23b592c3bd8d58379be9b1d2a490c21f6800
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-31 18:53:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  array.collect do |name|
+    badge_maker(name)
+  end 
+end 
+
+def assign_rooms(list)
+   i = 0
+  list.collect do |name|
+    i += 1
+    "Hello, #{name}! You'll be assigned to room #{i}!"
+    end
+end 
+
+def printer(attendees)
+  attendees.each do |name|
+  puts badge_maker(name)
+end 
+
+
+
